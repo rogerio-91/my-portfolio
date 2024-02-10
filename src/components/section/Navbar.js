79887aeb8c030105bfbba7a485a79bc62049cbca
Rogério Júnior <rogerio.rocha91@gmail.com>
2024-02-10 13:43:14 -0300

    feat: Add favicon

--- src/components/section/Navbar.js
+++ src/components/section/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import { FaInstagram, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaLinkedin, FaWhatsapp, } from "react-icons/fa";
 import Nav from 'react-bootstrap/Nav';
 
 const Navbar = () => {
