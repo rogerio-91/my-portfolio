c527e9e463d84d52bfb1b0f84f20a5b5a35851a7
Rogério Júnior <rogerio.rocha91@gmail.com>
2024-02-14 10:00:24 -0300

    Feat: Add interactivy to cards

--- src/components/elements/Card.js
+++ src/components/elements/Card.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Card.module.css'
 import ButtonB from '../elements/ButtonB';
 
 const Card = ({ img, title, tech, description, repository, site }) => {
+
+  const [info, setInfo] = useState(false); 
+
+  const infoOn = () => {
+    setInfo(true)
+  };
+
+  const infoOf = () =>{
+    setInfo(false)
+  }
+
+  
   return (
-    <div className={styles.card}>
-      <a href={site}>
+    <div onMouseLeave={infoOf} className={styles.card}>
+      <a onMouseEnter={infoOn} href={site}>
         <img src={img} alt='ERRO' />
       </a>
 
-      <section>
+      {info === true &&(
+        <section>
         <h3>{title}</h3>
         <p><strong>Tecnologia:</strong> {tech} </p>
         <p>{description}</p>
         <ButtonB text='Acesse o Repositorio' link={repository} />
       </section>
+      )
+
+      }
+
+      
     </div>
 
   )
